1393c804f7f567939185d49788851397f3eaadc0
amcneilly <amcneilly331@gmail.com>
2021-11-18 17:52:34 -0500

    Adding accompanist to create edge to edge experinece.

--- app/src/main/java/com/adammcneilly/pocketleague/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/pocketleague/MainActivity.kt
@@ -3,10 +3,20 @@ package com.adammcneilly.pocketleague
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
 import com.adammcneilly.pocketleague.core.ui.theme.PocketLeagueTheme
 import com.adammcneilly.pocketleague.teamlist.ui.TeamListScreen
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.statusBarsPadding
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,12 +25,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             PocketLeagueTheme {
-                Scaffold {
-                    TeamListScreen()
+                SetSystemBarsTransparent()
+
+                ProvideWindowInsets {
+                    Scaffold { paddingValues ->
+                        TeamListScreen(
+                            modifier = Modifier
+                                .padding(paddingValues)
+                                .statusBarsPadding()
+                        )
+                    }
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun SetSystemBarsTransparent() {
+        val systemUiController = rememberSystemUiController()
+        val useDarkIcons = !isSystemInDarkTheme()
+
+        SideEffect {
+            systemUiController.setSystemBarsColor(
+                color = Color.Transparent,
+                darkIcons = useDarkIcons
+            )
         }
     }
 }
--- app/src/main/java/com/adammcneilly/pocketleague/teamlist/ui/TeamListScreen.kt
+++ app/src/main/java/com/adammcneilly/pocketleague/teamlist/ui/TeamListScreen.kt
@@ -2,15 +2,18 @@ package com.adammcneilly.pocketleague.teamlist.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun TeamListScreen(
+    modifier: Modifier = Modifier,
     viewModel: TeamListViewModel = hiltViewModel(),
 ) {
     val viewState = viewModel.viewState.collectAsState()
 
     TeamListContent(
         viewState = viewState.value,
+        modifier = modifier,
     )
 }

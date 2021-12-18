ea1332b60e96f5f5e9143aae8dc16d7861091d0d
amcneilly <amcneilly331@gmail.com>
2021-12-17 20:42:18 -0500

    Passing eventId into EventOverviewScreen.

--- app/src/main/java/com/adammcneilly/pocketleague/eventoverview/ui/EventOverviewScreen.kt
+++ app/src/main/java/com/adammcneilly/pocketleague/eventoverview/ui/EventOverviewScreen.kt
@@ -7,9 +7,18 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 
 /**
+ * Navigation arguments that will be passed into an [EventOverviewScreen].
+ */
+data class EventOverviewNavArgs(
+    val eventId: String,
+)
+
+/**
  * Displays the [EventOverviewContent] with the view state pulled from the supplied [viewModel].
  */
-@Destination
+@Destination(
+    navArgsDelegate = EventOverviewNavArgs::class,
+)
 @Composable
 fun EventOverviewScreen(
     modifier: Modifier = Modifier,

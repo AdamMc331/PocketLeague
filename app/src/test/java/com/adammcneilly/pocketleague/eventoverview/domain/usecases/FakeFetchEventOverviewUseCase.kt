bfb4981e4ec50131f6856443dd9e5e93c1d0f87b
amcneilly <amcneilly331@gmail.com>
2022-01-16 15:44:09 -0500

    Adding more tests.

---
+++ app/src/test/java/com/adammcneilly/pocketleague/eventoverview/domain/usecases/FakeFetchEventOverviewUseCase.kt
@@ -0,0 +1,20 @@
+package com.adammcneilly.pocketleague.eventoverview.domain.usecases
+
+import com.adammcneilly.pocketleague.core.data.Result
+import com.adammcneilly.pocketleague.eventoverview.domain.models.EventOverview
+
+class FakeFetchEventOverviewUseCase : FetchEventOverviewUseCase {
+
+    private val mockEventResults: MutableMap<String, Result<EventOverview>> = mutableMapOf()
+
+    fun mockResultForEvent(
+        eventId: String,
+        result: Result<EventOverview>,
+    ) {
+        mockEventResults[eventId] = result
+    }
+
+    override suspend fun invoke(eventId: String): Result<EventOverview> {
+        return mockEventResults[eventId]!!
+    }
+}

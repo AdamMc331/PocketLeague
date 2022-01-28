65db5102b4933452e542086683d82a2d0699f457
amcneilly <amcneilly331@gmail.com>
2022-01-28 14:20:28 -0500

    Fixing tests so they pass.

--- app/src/androidTest/java/com/adammcneilly/pocketleague/fakes/FakeSeriesOverviewDisplayModel.kt
+++ app/src/androidTest/java/com/adammcneilly/pocketleague/fakes/FakeSeriesOverviewDisplayModel.kt
@@ -3,8 +3,12 @@ package com.adammcneilly.pocketleague.fakes
 import com.adammcneilly.pocketleague.seriesoverview.ui.SeriesOverviewDisplayModel
 
 val fakeSeriesOverviewDisplayModel = SeriesOverviewDisplayModel(
-    teamOne = fakeTeamOverviewDisplayModel,
-    teamTwo = fakeTeamOverviewDisplayModel,
+    teamOne = fakeTeamOverviewDisplayModel.copy(
+        name = "Team One",
+    ),
+    teamTwo = fakeTeamOverviewDisplayModel.copy(
+        name = "Team Two",
+    ),
     teamOneWins = 0,
     teamTwoWins = 0,
 )

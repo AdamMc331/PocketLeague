3dbcf0f88664951ef701e04d4d9199f2ad37b32a
amcneilly <amcneilly331@gmail.com>
2022-02-17 10:34:28 -0500

    More version gradle migrations.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,47 +1,61 @@
 object Versions {
+    // https://developer.android.com/jetpack/androidx/releases/compose
     const val compose = "1.1.0"
-//    ext {
-//        versions = [
-//            // https://developer.android.com/jetpack/androidx/releases/core
-//            ktxCore            : "1.7.0",
-//        // https://developer.android.com/jetpack/androidx/releases/appcompat
-//        appCompat          : "1.4.1",
-//        // https://developer.android.com/jetpack/androidx/releases/compose
-//        compose            : "1.1.0",
-//        // https://developer.android.com/jetpack/androidx/releases/lifecycle
-//        lifecycle          : "2.4.0",
-//        // https://developer.android.com/jetpack/androidx/releases/activity
-//        activityCompose    : "1.4.0",
-//        // https://github.com/junit-team/junit4/releases
-//        junit              : "4.13.2",
-//        // https://developer.android.com/jetpack/androidx/releases/test
-//        androidxTest       : "1.1.3",
-//        // https://developer.android.com/jetpack/androidx/releases/test
-//        espresso           : "3.4.0",
-//        // https://github.com/material-components/material-components-android/releases
-//        material           : "1.5.0",
-//        // https://developer.android.com/jetpack/androidx/releases/compose-material3
-//        composeMaterial3   : "1.0.0-alpha05",
-//        // https://github.com/google/truth/releases
-//        truth              : "1.1.3",
-//        // https://github.com/square/retrofit/releases
-//        retrofit           : "2.9.0",
-//        // https://github.com/square/okhttp/blob/master/CHANGELOG.md
-//        okhttp             : "4.9.3",
-//        // https://github.com/square/moshi/releases
-//        moshi              : "1.12.0",
-//        // https://github.com/google/accompanist/releases
-//        accompanist        : "0.20.3",
-//        // https://github.com/apollographql/apollo-android/releases
-//        apollo             : "2.5.11",
-//        // https://github.com/raamcosta/compose-destinations/releases
-//        composeDestinations: "0.9.4-beta",
-//        // https://developer.android.com/jetpack/androidx/releases/navigation
-//        composeNavigation  : "2.4.0",
-//        // https://github.com/Kotlin/kotlinx.coroutines/releases
-//        coroutines         : "1.6.0",
-//        // https://github.com/cashapp/turbine/releases
-//        turbine            : "0.7.0",
-//        ]
-//    }
+
+    // https://developer.android.com/jetpack/androidx/releases/core
+    const val ktxCore = "1.7.0"
+
+    // https://developer.android.com/jetpack/androidx/releases/core
+    const val appCompat = "1.4.1"
+
+    // https://developer.android.com/jetpack/androidx/releases/lifecycle
+    const val lifecycle = "2.4.0"
+
+    // https://developer.android.com/jetpack/androidx/releases/activity
+    const val activityCompose = "1.4.0"
+
+    // https://github.com/junit-team/junit4/releases
+    const val junit = "4.13.2"
+
+    // https://developer.android.com/jetpack/androidx/releases/test
+    const val androidxTest = "1.1.3"
+
+    // https://developer.android.com/jetpack/androidx/releases/test
+    const val espresso = "3.4.0"
+
+    // https://github.com/material-components/material-components-android/releases
+    const val material = "1.5.0"
+
+    // https://developer.android.com/jetpack/androidx/releases/compose-material3
+    const val composeMaterial3 = "1.0.0-alpha05"
+
+    // https://github.com/google/truth/releases
+    const val truth = "1.1.3"
+
+    // https://github.com/square/retrofit/releases
+    const val retrofit = "2.9.0"
+
+    // https://github.com/square/okhttp/blob/master/CHANGELOG.md
+    const val okhttp = "4.9.3"
+
+    // https://github.com/square/moshi/releases
+    const val moshi = "1.12.0"
+
+    // https://github.com/google/accompanist/releases
+    const val accompanist = "0.20.3"
+
+    // https://github.com/apollographql/apollo-android/releases
+    const val apollo = "3.1.0"
+
+    // https://github.com/raamcosta/compose-destinations/releases
+    const val composeDestinations = "0.9.4-beta"
+
+    // https://developer.android.com/jetpack/androidx/releases/navigation
+    const val composeNavigation = "2.4.0"
+
+    // https://github.com/Kotlin/kotlinx.coroutines/releases
+    const val coroutines = "1.6.0"
+
+    // https://github.com/cashapp/turbine/releases
+    const val turbine = "0.7.0"
 }

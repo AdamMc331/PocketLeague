b2f0f757fa4507b344ba4de8c2d03038c0614440
amcneilly <amcneilly331@gmail.com>
2022-02-18 19:32:53 -0500

    Removing stub code in iOS.

--- PocketLeagueiOS/PocketLeagueiOS/ContentView.swift
+++ PocketLeagueiOS/PocketLeagueiOS/ContentView.swift
@@ -33,20 +33,6 @@ class EventSummaryListviewModel: ObservableObject {
     @Published var eventNames: [String] = []
     
     func fetchUpcomingEvents() async {
-        do {
-            let eventResult = try await SmashGGEventSummaryService().fetchUpcomingEventSummaries(leagueSlug: "rlcs-2021-22-1")
-            
-            if let resultData = eventResult as? PLResultSuccess {
-                if let eventSummaries = resultData.data as? [Core_modelsEventSummary] {
-                    eventNames = eventSummaries.map {
-                        $0.tournamentName
-                    }
-                }
-            }
-            
-            print(eventNames)
-        } catch {
-            print(error)
-        }
+        eventNames = ["Testy", "McTest"]
     }
 }

de5ecff13082a40b38ce823b10aaff1c850d0b5d
amcneilly <amcneilly331@gmail.com>
2022-02-23 13:37:45 -0500

    Removing template code.

--- eventoverview/src/commonMain/kotlin/com/adammcneilly/pocketleague/eventoverview/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.adammcneilly.pocketleague.eventoverview
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- eventoverview/src/commonMain/kotlin/com/adammcneilly/pocketleague/eventoverview/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.adammcneilly.pocketleague.eventoverview
-
-expect class Platform() {
-    val platform: String
-}

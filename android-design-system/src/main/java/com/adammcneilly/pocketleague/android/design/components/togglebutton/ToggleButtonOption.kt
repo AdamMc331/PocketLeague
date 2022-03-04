b9dd542e00a40fc34453b407cc4090e671089534
amcneilly <amcneilly331@gmail.com>
2022-03-04 10:40:05 -0500

    Fixing detekt.

--- android-design-system/src/main/java/com/adammcneilly/pocketleague/android/design/components/togglebutton/ToggleButtonOption.kt
+++ android-design-system/src/main/java/com/adammcneilly/pocketleague/android/design/components/togglebutton/ToggleButtonOption.kt
@@ -3,7 +3,7 @@ package com.adammcneilly.pocketleague.android.design.components.togglebutton
 /**
  * Defines any option that can appear inside a [ToggleButtonRow].
  *
- * @param[text] The string that should be displayed to the user inside this button.
+ * @property[text] The string that should be displayed to the user inside this button.
  */
 data class ToggleButtonOption(
     val text: String,

11689e7a6a7cc90b10b6fb4bcc2def244107722a
amcneilly <amcneilly331@gmail.com>
2022-03-26 14:48:23 -0400

    Updating import.

--- app/src/test/java/com/adammcneilly/pocketleague/teamdetail/ui/TeamDetailDisplayModelTest.kt
+++ app/src/test/java/com/adammcneilly/pocketleague/teamdetail/ui/TeamDetailDisplayModelTest.kt
@@ -4,6 +4,7 @@ import com.adammcneilly.pocketleague.core.ui.FakeFlagResProvider
 import com.adammcneilly.pocketleague.shared.core.models.Player
 import com.adammcneilly.pocketleague.shared.core.models.Team
 import com.adammcneilly.pocketleague.shared.core.ui.UIImage
+import com.adammcneilly.pocketleague.teamdetail.ui.toDetailDisplayModel
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 

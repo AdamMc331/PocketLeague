88723bc603cdb8c90b6489b22443d7b9171e124e
amcneilly <amcneilly331@gmail.com>
2022-03-27 16:12:23 -0400

    Moving event-implementation into shared module.

---
+++ shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/eventoverview/domain/GetEventOverviewUseCaseImpl.kt
@@ -0,0 +1,28 @@
+package com.adammcneilly.pocketleague.shared.eventoverview.domain
+
+import com.adammcneilly.pocketleague.shared.data.Result
+import com.adammcneilly.pocketleague.shared.data.event.EventRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+/**
+ * A concrete implementation of [GetEventOverviewUseCase] that will request information
+ * from the supplied [repository].
+ */
+class GetEventOverviewUseCaseImpl(
+    private val repository: EventRepository,
+) : GetEventOverviewUseCase {
+
+    override fun invoke(eventId: String): Flow<GetEventOverviewUseCase.Result> {
+        return repository.fetchEventOverview(eventId).map { repoResult ->
+            when (repoResult) {
+                is Result.Success -> {
+                    GetEventOverviewUseCase.Result.Success(repoResult.data)
+                }
+                is Result.Error -> {
+                    GetEventOverviewUseCase.Result.Error(repoResult.error.message)
+                }
+            }
+        }
+    }
+}

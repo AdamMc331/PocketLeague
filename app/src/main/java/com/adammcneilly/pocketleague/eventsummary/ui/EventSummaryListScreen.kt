8ce9204a0291c18ae4128c0869cbee964cad0e92
amcneilly <amcneilly331@gmail.com>
2022-03-28 10:43:38 -0400

    Adding import.

--- app/src/main/java/com/adammcneilly/pocketleague/eventsummary/ui/EventSummaryListScreen.kt
+++ app/src/main/java/com/adammcneilly/pocketleague/eventsummary/ui/EventSummaryListScreen.kt
@@ -3,6 +3,7 @@ package com.adammcneilly.pocketleague.eventsummary.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.adammcneilly.pocketleague.eventsummary.ui.EventSummaryListContent

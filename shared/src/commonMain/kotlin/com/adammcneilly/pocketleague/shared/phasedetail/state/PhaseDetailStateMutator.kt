8dbe8433588eb853004ed20de78256f8d4d2b7b3
amcneilly <amcneilly331@gmail.com>
2022-04-03 18:39:46 -0400

    Fetching phase detail via mutator.

--- shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/phasedetail/state/PhaseDetailStateMutator.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/phasedetail/state/PhaseDetailStateMutator.kt
@@ -1,19 +1,24 @@
 package com.adammcneilly.pocketleague.shared.phasedetail.state
 
+import com.adammcneilly.pocketleague.shared.core.models.PhaseDetail
+import com.adammcneilly.pocketleague.shared.core.ui.UIText
+import com.adammcneilly.pocketleague.shared.phasedetail.domain.GetPhaseDetailUseCase
+import com.adammcneilly.pocketleague.shared.phasedetail.ui.PhaseDetailDisplayModel
 import com.tunjid.mutator.Mutation
 import com.tunjid.mutator.coroutines.stateFlowMutator
 import com.tunjid.mutator.coroutines.toMutationStream
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 /**
  * Create a [stateFlowMutator] that will be used to manage our [PhaseDetailViewState].
  */
 fun phaseDetailStateMutator(
     scope: CoroutineScope,
+    getPhaseDetailUseCase: GetPhaseDetailUseCase,
 ) = stateFlowMutator<PhaseDetailAction, PhaseDetailViewState>(
     scope = scope,
     initialState = PhaseDetailViewState(),
@@ -22,26 +27,62 @@ fun phaseDetailStateMutator(
             when (val action = type()) {
                 is PhaseDetailAction.FetchPhaseDetail ->
                     action.flow
-                        .fetchPhaseDetailMutations()
+                        .fetchPhaseDetailMutations(getPhaseDetailUseCase)
             }
         }
     }
 )
 
-private fun Flow<PhaseDetailAction.FetchPhaseDetail>.fetchPhaseDetailMutations(): Flow<Mutation<PhaseDetailViewState>> {
+private fun Flow<PhaseDetailAction.FetchPhaseDetail>.fetchPhaseDetailMutations(
+    getPhaseDetailUseCase: GetPhaseDetailUseCase,
+): Flow<Mutation<PhaseDetailViewState>> {
     return this.flatMapLatest { action ->
-        flow {
-            emitLoading()
-
-            // Loading coming soon
-        }
+        getPhaseDetailUseCase
+            .invoke(action.phaseId)
+            .map { result ->
+                when (result) {
+                    is GetPhaseDetailUseCase.Result.Success -> {
+                        successMutation(
+                            phaseDetail = result.phaseDetail,
+                        )
+                    }
+                    is GetPhaseDetailUseCase.Result.Error -> {
+                        errorMutation()
+                    }
+                }
+            }
+            .onStart {
+                emit(loadingMutation())
+            }
     }
 }
 
-private suspend fun FlowCollector<Mutation<PhaseDetailViewState>>.emitLoading() = this.emit(
-    Mutation {
-        copy(
-            showLoading = true,
-        )
-    }
-)
+private fun successMutation(
+    phaseDetail: PhaseDetail,
+) = Mutation<PhaseDetailViewState> {
+    copy(
+        showLoading = false,
+        phase = phaseDetail.toDisplayModel(),
+    )
+}
+
+private fun loadingMutation() = Mutation<PhaseDetailViewState> {
+    copy(
+        showLoading = true,
+    )
+}
+
+private fun PhaseDetail.toDisplayModel(): PhaseDetailDisplayModel {
+    return PhaseDetailDisplayModel(
+        phaseName = this.name,
+    )
+}
+
+private fun errorMutation() = Mutation<PhaseDetailViewState> {
+    copy(
+        showLoading = false,
+        errorMessage = UIText.StringText(
+            "Fetching phase detail failed.",
+        ),
+    )
+}

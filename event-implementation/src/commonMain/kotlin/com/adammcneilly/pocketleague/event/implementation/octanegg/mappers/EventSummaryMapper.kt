79231807098c5ec6ddd7218cb40718f0b73580cc
amcneilly <amcneilly331@gmail.com>
2022-04-11 09:37:05 -0400

    Requesting data from Octane.gg!

--- event-implementation/src/commonMain/kotlin/com/adammcneilly/pocketleague/event/implementation/octanegg/mappers/EventSummaryMapper.kt
+++ event-implementation/src/commonMain/kotlin/com/adammcneilly/pocketleague/event/implementation/octanegg/mappers/EventSummaryMapper.kt
@@ -2,8 +2,9 @@ package com.adammcneilly.pocketleague.event.implementation.octanegg.mappers
 
 import com.adammcneilly.pocketleague.core.models.EventSummary
 import com.adammcneilly.pocketleague.event.implementation.octanegg.dtos.EventDTO
-import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 /**
  * Converts an [EventDTO] from octane.gg into an [EventSummary].
@@ -14,7 +15,7 @@ fun EventDTO.toEventSummary(): EventSummary {
         eventName = this.name,
         tournamentName = this.name,
         tournamentImageUrl = this.image,
-        startDate = LocalDateTime.parse(this.startDate),
+        startDate = Instant.parse(this.startDate).toLocalDateTime(TimeZone.UTC),
         timeZone = TimeZone.UTC,
         numEntrants = null,
         isOnline = false,

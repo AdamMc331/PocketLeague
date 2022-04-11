1712b3cdfd5ee128a28475db0e22355337f1b7e3
Adam McNeilly <amcneilly331@gmail.com>
2022-04-11 10:33:19 -0400

    Adding Octane.GG API. (#85)

---
+++ event-implementation/src/commonMain/kotlin/com/adammcneilly/pocketleague/event/implementation/octanegg/dtos/StageDTO.kt
@@ -0,0 +1,21 @@
+package com.adammcneilly.pocketleague.event.implementation.octanegg.dtos
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * A data class mapping to a stage from the octane.gg API.
+ */
+@Serializable
+data class StageDTO(
+    @SerialName("_id")
+    val id: Int,
+    val endDate: String? = null,
+    val name: String? = null,
+    val startDate: String? = null,
+    val prize: PrizeDTO? = null,
+    val location: LocationDTO? = null,
+    val lan: Boolean? = null,
+    val liquipedia: String? = null,
+    val region: String? = null,
+)

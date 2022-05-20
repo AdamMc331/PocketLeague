ee4b165270f5242e11cb03058e3773ed0e97b5d4
adammc331 <amcneilly331@gmail.com>
2022-05-20 17:57:17 -0400

    Fixing docs.

--- shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/screens/eventdetail/EventDetailViewState.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/screens/eventdetail/EventDetailViewState.kt
@@ -6,6 +6,7 @@ import com.adammcneilly.pocketleague.shared.screens.ScreenState
  * Defines the UI state of the event detail screen.
  *
  * @property[showLoading] True if we should show some loading indicator on the screen, false otherwise.
+ * @property[eventId] The unique identifier of the event we are showing detailed information for.
  */
 data class EventDetailViewState(
     val showLoading: Boolean = true,

7957c5ae40e8889a7ebd433b2b3cdfd95c0c5bd7
adammc331 <amcneilly331@gmail.com>
2022-05-23 14:14:39 -0400

    Adding core-displaymodels module.

---
+++ core-displaymodels/src/commonTest/kotlin/com/adammcneilly/pocketleague/core/displaymodels/EventSummaryDisplayModelTest.kt
@@ -0,0 +1,32 @@
+package com.adammcneilly.pocketleague.core.displaymodels
+
+import com.adammcneilly.pocketleague.core.models.Event
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class EventSummaryDisplayModelTest {
+
+    @Test
+    fun convertFromEvent() {
+        val testEvent = Event(
+            id = "1234",
+            name = "Test Event",
+            startDate = Instant.parse("2022-01-01T12:00:00Z").toLocalDateTime(TimeZone.currentSystemDefault()),
+            endDate = Instant.parse("2022-01-02T12:00:00Z").toLocalDateTime(TimeZone.currentSystemDefault()),
+            imageUrl = "Test Image URL",
+        )
+
+        val displayModel = testEvent.toSummaryDisplayModel()
+
+        with(displayModel) {
+            assertEquals("1234", eventId)
+            assertEquals("Test Image URL", lightThemeImageUrl)
+            assertEquals("Test Image URL", darkThemeImageUrl)
+            assertEquals("Jan 01, 2022", eventStartDate)
+            assertEquals("Test Event", eventName)
+        }
+    }
+}

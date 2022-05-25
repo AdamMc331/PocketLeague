9a58b76808c7403808e169fafcc6e53f8ee08d87
adammc331 <amcneilly331@gmail.com>
2022-05-25 16:11:30 -0400

    Showing team logos on match detail.

--- app/src/main/java/com/adammcneilly/pocketleague/ui/MatchDetail.kt
+++ app/src/main/java/com/adammcneilly/pocketleague/ui/MatchDetail.kt
@@ -1,28 +1,34 @@
 package com.adammcneilly.pocketleague.ui
 
 import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Divider
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.adammcneilly.pocketleague.core.displaymodels.GameDetailDisplayModel
 import com.adammcneilly.pocketleague.core.displaymodels.GameTeamResultDisplayModel
 import com.adammcneilly.pocketleague.core.displaymodels.MatchDetailDisplayModel
 import com.adammcneilly.pocketleague.core.displaymodels.MatchTeamResultDisplayModel
 import com.adammcneilly.pocketleague.core.displaymodels.TeamOverviewDisplayModel
 import com.adammcneilly.pocketleague.ui.theme.PocketLeagueTheme
+import com.google.accompanist.placeholder.material.placeholder
 
 /**
  * Renders the [displayModel] to show detailed information about a match between two teams.
@@ -68,35 +74,50 @@ private fun GameList(
 
 @Composable
 private fun TeamResultsOverview(displayModel: MatchDetailDisplayModel) {
-    CompositionLocalProvider(
-        LocalTextStyle provides MaterialTheme.typography.headlineMedium,
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(24.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-        ) {
-            Text(
-                text = displayModel.blueTeamResult.team.name,
-            )
+        MatchTeamResultCell(displayModel = displayModel.blueTeamResult)
 
-            Text(
-                text = displayModel.blueTeamResult.score,
-            )
+        MatchTeamResultCell(displayModel = displayModel.orangeTeamResult)
+    }
+}
 
-            Text(
-                text = ":",
-            )
-
-            Text(
-                text = displayModel.orangeTeamResult.score,
-            )
-
-            Text(
-                text = displayModel.orangeTeamResult.team.name,
-            )
+@Composable
+private fun MatchTeamResultCell(
+    displayModel: MatchTeamResultDisplayModel,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        val imageUrl = if (isSystemInDarkTheme()) {
+            displayModel.team.darkThemeImageUrl
+        } else {
+            displayModel.team.lightThemeImageUrl
         }
+
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .build(),
+            contentDescription = "Team Image",
+            modifier = Modifier
+                .size(72.dp)
+                .placeholder(
+                    visible = imageUrl == null,
+                    shape = CircleShape,
+                    color = MaterialTheme.colorScheme.inverseSurface,
+                ),
+        )
+
+        Text(
+            text = displayModel.score,
+            style = MaterialTheme.typography.headlineMedium,
+        )
     }
 }
 

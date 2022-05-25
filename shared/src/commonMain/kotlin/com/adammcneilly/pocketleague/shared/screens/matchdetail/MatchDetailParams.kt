26a657cfb8797aa61c12e6dd958e6c33d3086b93
adammc331 <amcneilly331@gmail.com>
2022-05-24 20:58:29 -0400

    fixing docs.

--- shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/screens/matchdetail/MatchDetailParams.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/screens/matchdetail/MatchDetailParams.kt
@@ -1,12 +1,11 @@
 package com.adammcneilly.pocketleague.shared.screens.matchdetail
 
-import com.adammcneilly.pocketleague.core.models.Match
 import com.adammcneilly.pocketleague.shared.screens.ScreenParams
 
 /**
  * Parameters that will be passed into the match detail screen.
  *
- * @property[match] The [Match] entity to request detailed information for.
+ * @property[matchId] The The identifier of the match we want to show detailed information for.
  */
 data class MatchDetailParams(
     val matchId: String = "",

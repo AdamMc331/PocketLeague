058f216106f9176e8347fea8dc6a5398d4e3982a
adammc331 <amcneilly331@gmail.com>
2022-05-26 16:23:34 -0400

    Bolding text and showing trophy on game list item.

--- app/src/main/java/com/adammcneilly/pocketleague/ui/GameListItem.kt
+++ app/src/main/java/com/adammcneilly/pocketleague/ui/GameListItem.kt
@@ -5,10 +5,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.EmojiEvents
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.adammcneilly.pocketleague.core.displaymodels.GameDetailDisplayModel
@@ -30,19 +40,67 @@ fun GameListItem(
             .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-
-        Text(
-            text = displayModel.blueTeamResult.score,
-        )
+        TeamScore(displayModel = displayModel.blueTeamResult)
 
         Text(
             text = displayModel.map,
         )
 
-        Text(
-            text = displayModel.orangeTeamResult.score,
-        )
+        TeamScore(displayModel = displayModel.orangeTeamResult)
     }
+}
+
+@Composable
+private fun TeamScore(
+    displayModel: GameTeamResultDisplayModel,
+) {
+    val isWinner = displayModel.winner
+
+    val fontWeight: FontWeight? = if (isWinner) {
+        FontWeight.Bold
+    } else {
+        null
+    }
+
+    val inlineContentId = "inlineContent"
+
+    val scoreText = buildAnnotatedString {
+        append(displayModel.score)
+
+        if (isWinner) {
+            append(" ")
+
+            appendInlineContent(inlineContentId, "[trophy]")
+        }
+    }
+
+    val inlineContent: Map<String, InlineTextContent> = if (isWinner) {
+        mapOf(
+            Pair(
+                inlineContentId,
+                InlineTextContent(
+                    Placeholder(
+                        width = LocalTextStyle.current.fontSize,
+                        height = LocalTextStyle.current.fontSize,
+                        placeholderVerticalAlign = PlaceholderVerticalAlign.TextCenter,
+                    )
+                ) {
+                    Icon(
+                        Icons.Default.EmojiEvents,
+                        contentDescription = null,
+                    )
+                }
+            )
+        )
+    } else {
+        mapOf()
+    }
+
+    Text(
+        text = scoreText,
+        fontWeight = fontWeight,
+        inlineContent = inlineContent,
+    )
 }
 
 @Preview(

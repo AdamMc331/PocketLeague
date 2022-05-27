e785226d339fd043bb5a3534b25034a45593d594
adammc331 <amcneilly331@gmail.com>
2022-05-27 12:29:09 -0400

    Fixing docs.

--- core-models/src/commonMain/kotlin/com/adammcneilly/pocketleague/core/models/GameTeamResult.kt
+++ core-models/src/commonMain/kotlin/com/adammcneilly/pocketleague/core/models/GameTeamResult.kt
@@ -8,6 +8,7 @@ package com.adammcneilly.pocketleague.core.models
  * @property[matchWinner] True if this team won the match between both teams.
  * @property[team] Detailed information about the [Team] playing in the match.
  * @property[teamStats] The detailed [Stats] for this [team] in the game.
+ * @property[players] The players for this [team] and how they performed in this game.
  */
 data class GameTeamResult(
     val goals: Int = -1,

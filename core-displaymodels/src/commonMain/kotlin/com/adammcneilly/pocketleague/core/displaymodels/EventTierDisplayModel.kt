e2fde3b880386daae94098398fa53c14344511d3
adammc331 <amcneilly331@gmail.com>
2022-05-28 14:40:00 -0400

    Adding display model for event tier.

---
+++ core-displaymodels/src/commonMain/kotlin/com/adammcneilly/pocketleague/core/displaymodels/EventTierDisplayModel.kt
@@ -0,0 +1,41 @@
+package com.adammcneilly.pocketleague.core.displaymodels
+
+import com.adammcneilly.pocketleague.core.models.EventTier
+
+/**
+ * Displays information about an [EventTier] in a user friendly fashion.
+ */
+data class EventTierDisplayModel(
+    val name: String,
+    val description: String,
+)
+
+fun EventTier.toDisplayModel(): EventTierDisplayModel {
+    val name = "${this.name}-Tier"
+
+    val description = when (this) {
+        EventTier.S -> {
+            "S-Tier events include only the world's elite teams."
+        }
+        EventTier.A -> {
+            "A-Tier events include top competition, but often have open qualifiers."
+        }
+        EventTier.B -> {
+            "B-Tier events have moderate impact and prize pools with strong competition."
+        }
+        EventTier.C -> {
+            "C-Tier events focus on a bubble scene and have small prize pools."
+        }
+        EventTier.D -> {
+            "D-Tier events have small prize pools and are outside the coverage of most RLCS events."
+        }
+        EventTier.Unknown -> {
+            "The Tier of this event is unreported."
+        }
+    }
+
+    return EventTierDisplayModel(
+        name = name,
+        description = description,
+    )
+}
---
+++ core-models/src/commonMain/kotlin/com/adammcneilly/pocketleague/core/models/EventTier.kt
@@ -0,0 +1,16 @@
+package com.adammcneilly.pocketleague.core.models
+
+/**
+ * A tier defines the notoriety of an event. This app uses the tiers defined and explained by Liquipedia,
+ * which you can read about here:
+ *
+ * https://liquipedia.net/rocketleague/Portal:Tournaments
+ */
+enum class EventTier {
+    S,
+    A,
+    B,
+    C,
+    D,
+    Unknown,
+}

93384dd699ef13df4ff4ea01682f4564af88d558
adammc331 <amcneilly331@gmail.com>
2022-05-30 21:39:07 -0400

    Fixing static analysis checks.

--- composables/src/commonMain/kotlin/com/adammcneilly/pocketleague/composables/TopBar.kt
+++ composables/src/commonMain/kotlin/com/adammcneilly/pocketleague/composables/TopBar.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
-
 /**
  * This shows the top toolbar at the top of an application.
  */

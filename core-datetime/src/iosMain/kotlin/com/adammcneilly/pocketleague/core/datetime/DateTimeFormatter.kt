bca50e0d7d3ec74d022903920fecfff0f13259eb
adammc331 <amcneilly331@gmail.com>
2022-06-04 22:09:07 -0700

    Fixing docs.

--- core-datetime/src/iosMain/kotlin/com/adammcneilly/pocketleague/core/datetime/DateTimeFormatter.kt
+++ core-datetime/src/iosMain/kotlin/com/adammcneilly/pocketleague/core/datetime/DateTimeFormatter.kt
@@ -1,5 +1,8 @@
 package com.adammcneilly.pocketleague.core.datetime
 
+/**
+ * Creates an [IosDateTimeFormatter] to be used on the iOS platform.
+ */
 actual fun dateTimeFormatter(): DateTimeFormatter {
     return IosDateTimeFormatter()
 }

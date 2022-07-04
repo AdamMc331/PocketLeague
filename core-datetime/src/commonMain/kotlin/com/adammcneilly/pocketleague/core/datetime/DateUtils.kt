263634a468c084f114c195101de580a1c2eb5f6f
adammc331 <amcneilly331@gmail.com>
2022-07-04 16:39:52 -0400

    Fixing documentation.

--- core-datetime/src/commonMain/kotlin/com/adammcneilly/pocketleague/core/datetime/DateUtils.kt
+++ core-datetime/src/commonMain/kotlin/com/adammcneilly/pocketleague/core/datetime/DateUtils.kt
@@ -6,12 +6,15 @@ import kotlinx.datetime.Instant
 /**
  * Utility methods for a date that don't have to be resolved in a platform specific way.
  *
- * TODO: Consider if we can move this inside our DateTimeFormatter interface?
+ * NOTE: Consider if we can move this inside our DateTimeFormatter interface?
  */
 object DateUtils {
     private const val MINUTES_IN_HOUR = 60
     private const val HOURS_IN_DAY = 24
 
+    /**
+     * Given an [instant], convert it to a relative timestamp such as 5m ago, or 5d ago.
+     */
     fun getRelativeTimestamp(instant: Instant): String {
         val now = Clock.System.now()
 

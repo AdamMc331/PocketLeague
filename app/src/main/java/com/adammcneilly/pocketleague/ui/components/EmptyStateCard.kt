fa7ed69ea7d8ff681cef0d61a73f5ac4f4441c8a
adammc331 <amcneilly331@gmail.com>
2022-07-08 12:44:05 -0400

    Tweaking UI.

--- app/src/main/java/com/adammcneilly/pocketleague/ui/components/EmptyStateCard.kt
+++ app/src/main/java/com/adammcneilly/pocketleague/ui/components/EmptyStateCard.kt
@@ -1,10 +1,12 @@
 package com.adammcneilly.pocketleague.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 
 /**
  * Creates a [Card] composable that renders the supplied [text]. Useful for empty and error states.
@@ -21,7 +23,9 @@ fun EmptyStateCard(
     ) {
         Text(
             text = text,
-            modifier = textModifier,
+            modifier = textModifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
         )
     }
 }

28f373dfe1ecd8bf71dc3db4c7158ae08469fc73
adammc331 <amcneilly331@gmail.com>
2022-09-18 21:45:19 -0400

    Using updated app bar.

--- app/src/main/java/com/adammcneilly/pocketleague/ui/composables/bars/TopBar.kt
+++ app/src/main/java/com/adammcneilly/pocketleague/ui/composables/bars/TopBar.kt
@@ -1,7 +1,8 @@
 package com.adammcneilly.pocketleague.ui.composables.bars
 
-import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -11,12 +12,13 @@ import androidx.compose.ui.Modifier
  * @param[title] The text to display inside this toolbar.
  * @param[modifier] Optional modifications to perform on this component.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(
     title: String,
     modifier: Modifier = Modifier,
 ) {
-    SmallTopAppBar(
+    TopAppBar(
         title = {
             Text(
                 text = title,

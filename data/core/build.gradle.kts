cc06a17caebcb6ba36ff94c7e569053b12d9f9be
adammc331 <amcneilly331@gmail.com>
2022-09-24 22:27:19 -0400

    Trying to compile with iOS targets.

--- data/core/build.gradle.kts
+++ data/core/build.gradle.kts
@@ -12,5 +12,42 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
+        maybeCreate("iosX64Main")
+        maybeCreate("iosArm64Main")
+        maybeCreate("iosSimulatorArm64Main")
+        maybeCreate("iosMain").apply {
+            dependsOn(commonMain)
+            getAt("iosX64Main").dependsOn(this)
+            getAt("iosArm64Main").dependsOn(this)
+            getAt("iosSimulatorArm64Main").dependsOn(this)
+        }
+        maybeCreate("iosX64Test")
+        maybeCreate("iosArm64Test")
+        maybeCreate("iosSimulatorArm64Test")
+        maybeCreate("iosTest").apply {
+            dependsOn(commonTest)
+            getAt("iosX64Test").dependsOn(this)
+            getAt("iosArm64Test").dependsOn(this)
+            getAt("iosSimulatorArm64Test").dependsOn(this)
+        }
+    }
+}
+
+project.extensions.findByType(org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension::class.java)?.apply {
+    if (project.findProperty("ios") == "true") {
+        listOf(
+            iosX64(),
+            iosArm64(),
+            iosSimulatorArm64()
+        ).forEach {
+            it.binaries.framework {
+                baseName = project.name
+            }
+        }
+    }
+    if (project.findProperty("js") == "true") {
+        js(IR) {
+            browser()
+        }
     }
 }

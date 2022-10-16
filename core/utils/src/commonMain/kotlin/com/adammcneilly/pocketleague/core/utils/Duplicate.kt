91a17bdefb41b9b6903070fdb739abd43ac88623
AdamMc331 <amcneilly331@gmail.com>
2022-10-15 21:38:53 -0400

    Fixing docs.

--- core/utils/src/commonMain/kotlin/com/adammcneilly/pocketleague/core/utils/Duplicate.kt
+++ core/utils/src/commonMain/kotlin/com/adammcneilly/pocketleague/core/utils/Duplicate.kt
@@ -1,7 +1,7 @@
 package com.adammcneilly.pocketleague.core.utils
 
 /**
- * Given some [item], convert it into a list of size [count].
+ * Given some item, duplicate it into a list of size [count].
  */
 inline fun <T> T.duplicate(count: Int): List<T> {
     return (1..count).map {

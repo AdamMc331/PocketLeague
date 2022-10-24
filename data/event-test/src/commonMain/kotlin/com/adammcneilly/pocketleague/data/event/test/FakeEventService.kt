93ed8176f5cf86751916e918bce9ebe8e007856a
AdamMc331 <amcneilly331@gmail.com>
2022-10-23 22:35:45 -0400

    Adding event-test module.

---
+++ data/event-test/src/commonMain/kotlin/com/adammcneilly/pocketleague/data/event/test/FakeEventService.kt
@@ -0,0 +1,8 @@
+package com.adammcneilly.pocketleague.data.event.test
+
+import com.adammcneilly.pocketleague.data.event.EventService
+
+class FakeEventService : EventService {
+
+    // Coming soon.
+}

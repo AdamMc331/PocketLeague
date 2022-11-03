866e5c6b8639fbc5225697c08f79552801ba00f9
adammc331 <amcneilly331@gmail.com>
2022-11-02 21:06:28 -0400

    Adding stat comparison card.

---
+++ android/design-system/src/main/java/com/adammcneilly/pocketleague/android/designsystem/stats/CoreStatsComparisonCard.kt
@@ -0,0 +1,97 @@
+package com.adammcneilly.pocketleague.android.designsystem.stats
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adammcneilly.pocketleague.android.designsystem.theme.PocketLeagueTheme
+import com.adammcneilly.pocketleague.core.displaymodels.CoreStatsDisplayModel
+
+/**
+ * Render a comparison of [blueTeamStats] and [orangeTeamStats] for the
+ * various core stats.
+ */
+@Composable
+fun CoreStatsComparisonCard(
+    blueTeamStats: CoreStatsDisplayModel,
+    orangeTeamStats: CoreStatsDisplayModel,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            Text(
+                text = "Goals",
+            )
+
+            StatComparison(
+                blueTeamValue = blueTeamStats.goals,
+                orangeTeamValue = orangeTeamStats.goals,
+            )
+
+            Text(
+                text = "Assists",
+            )
+
+            StatComparison(
+                blueTeamValue = blueTeamStats.assists,
+                orangeTeamValue = orangeTeamStats.assists,
+            )
+
+            Text(
+                text = "Saves",
+            )
+
+            StatComparison(
+                blueTeamValue = blueTeamStats.saves,
+                orangeTeamValue = orangeTeamStats.saves,
+            )
+        }
+    }
+}
+
+@Preview(
+    name = "Night Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Preview(
+    name = "Day Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Composable
+private fun CoreStatsComparisonCardPreview() {
+    PocketLeagueTheme {
+        CoreStatsComparisonCard(
+            blueTeamStats = CoreStatsDisplayModel(
+                score = 1000,
+                goals = 7,
+                assists = 4,
+                saves = 3,
+                shots = 1,
+            ),
+            orangeTeamStats = CoreStatsDisplayModel(
+                score = 8729374,
+                goals = 3,
+                assists = 8,
+                saves = 0,
+                shots = 5,
+            ),
+            modifier = Modifier
+                .padding(16.dp),
+        )
+    }
+}

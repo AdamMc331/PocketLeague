6378ace8deabad86f5c853c7087c318b719b534d
adammc331 <amcneilly331@gmail.com>
2022-11-16 20:53:10 -0500

    Adding paparazzi tests.

--- android/design-system/src/main/java/com/adammcneilly/pocketleague/android/designsystem/teamselection/TeamSelectionListItem.kt
+++ android/design-system/src/main/java/com/adammcneilly/pocketleague/android/designsystem/teamselection/TeamSelectionListItem.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.StarBorder
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme

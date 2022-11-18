c4884863e452e35dfeacb7fed996b461084d53b5
adammc331 <amcneilly331@gmail.com>
2022-11-18 12:49:19 -0500

    Adding widgets module.

---
+++ android/widgets/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.adammcneilly.pocketleague.widgets"
+    compileSdk = AndroidConfig.compileSDK
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSDK
+        targetSdk = AndroidConfig.targetSDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+}

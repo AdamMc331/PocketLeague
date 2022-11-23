aa201e58a66f414d2d646b68bbfd03085be6ba9b
AdamMc331 <amcneilly331@gmail.com>
2022-11-22 20:16:16 -0500

    Shortening comment.

--- app/src/androidTest/java/com/adammcneilly/pocketleague/MainActivityTest.kt
+++ app/src/androidTest/java/com/adammcneilly/pocketleague/MainActivityTest.kt
@@ -12,7 +12,7 @@ class MainActivityTest {
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    @Ignore("Need to differentiate text values for recent matches & ongoing events from test data. But since this test doesn't really do anything yet I'm just ignoring for now.")
+    @Ignore("Need to differentiate text values for recent matches & ongoing events from test data.")
     fun renderFeedScreen() {
         composeTestRule
             .onNodeWithText("RLCS 2021-22 World Championship")

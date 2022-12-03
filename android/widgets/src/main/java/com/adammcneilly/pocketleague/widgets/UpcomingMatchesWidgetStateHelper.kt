0d8852cd5859ef6feef7ee8697456b9d673802a3
adammc331 <amcneilly331@gmail.com>
2022-12-03 12:11:26 -0500

    Removing classes we didn't end up using.

--- android/widgets/src/main/java/com/adammcneilly/pocketleague/widgets/UpcomingMatchesWidgetStateHelper.kt
+++
@@ -1,3 +0,0 @@
-package com.adammcneilly.pocketleague.widgets
-
-object UpcomingMatchesWidgetStateHelper
--- android/widgets/src/main/java/com/adammcneilly/pocketleague/widgets/UpcomingMatchesWidgetViewState.kt
+++
@@ -1,7 +0,0 @@
-package com.adammcneilly.pocketleague.widgets
-
-import com.adammcneilly.pocketleague.core.displaymodels.MatchDetailDisplayModel
-
-data class UpcomingMatchesWidgetViewState(
-    val matches: List<MatchDetailDisplayModel> = emptyList(),
-)

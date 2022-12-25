7d93e8f078ed694928729a46b707b809e598e521
AdamMc331 <amcneilly331@gmail.com>
2022-12-25 18:23:06 -0500

    Creating FeedPresenter.

--- feature/feed/build.gradle.kts
+++ feature/feed/build.gradle.kts
@@ -9,8 +9,13 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation(project(":core:datetime"))
                 implementation(project(":core:displaymodels"))
                 implementation(project(":core:feature"))
+                implementation(project(":core:models"))
+                implementation(project(":data:event"))
+                implementation(project(":data:match"))
+                implementation(libs.kotlinx.coroutines.core)
             }
         }
         val commonTest by getting {

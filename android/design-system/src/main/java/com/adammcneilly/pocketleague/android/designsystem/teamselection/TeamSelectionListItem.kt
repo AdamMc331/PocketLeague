04b0fb3e012ec564f194548f7b789ddb7e86ba59
kanake10 <10kanake@gmail.com>
2023-02-07 03:59:18 +0300

    fix failing lint ci

--- android/design-system/src/main/java/com/adammcneilly/pocketleague/android/designsystem/teamselection/TeamSelectionListItem.kt
+++ android/design-system/src/main/java/com/adammcneilly/pocketleague/android/designsystem/teamselection/TeamSelectionListItem.kt
@@ -10,10 +10,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Icon
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable

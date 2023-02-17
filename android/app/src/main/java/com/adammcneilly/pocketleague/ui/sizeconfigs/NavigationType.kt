dee9b8d8463e9930b419545f818cc8540f924df4
Adam McNeilly <amcneilly331@gmail.com>
2023-02-17 13:19:18 -0500

    Fixing static analysis checks.

--- android/app/src/main/java/com/adammcneilly/pocketleague/ui/sizeconfigs/NavigationType.kt
+++ android/app/src/main/java/com/adammcneilly/pocketleague/ui/sizeconfigs/NavigationType.kt
@@ -3,11 +3,6 @@ package com.adammcneilly.pocketleague.ui.sizeconfigs
 /**
  * The [NavigationType] defines how a user can navigate within screens at the base level
  * of the application.
- *
- * @property[BOTTOM_NAVIGATION] When selected, the user will see navigation tabs at
- * the bottom of their screen.
- * @property[NAVIGATION_RAIL] When selected, the user will see navigation tabs on the side of
- * their screen.
  */
 enum class NavigationType {
     BOTTOM_NAVIGATION,

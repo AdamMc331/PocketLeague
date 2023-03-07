3fdd1fc8fab935ffbd85d0df482dfc865e7d13d2
Adam McNeilly <amcneilly331@gmail.com>
2023-03-06 20:57:13 -0500

    Adding paparazzi tests for CircleTeamLogo.  (#270)

---
+++ android/design-system/src/test/java/com/adammcneilly/pocketleague/android/designsystem/team/CircleTeamLogoPaparazziTest.kt
@@ -0,0 +1,39 @@
+package com.adammcneilly.pocketleague.android.designsystem.team
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import app.cash.paparazzi.Paparazzi
+import com.adammcneilly.pocketleague.android.designsystem.snapshotScreen
+import com.adammcneilly.pocketleague.core.displaymodels.ThemedImageURL
+import com.adammcneilly.pocketleague.core.displaymodels.test.TestDisplayModel
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class CircleTeamLogoPaparazziTest {
+
+    @get:Rule
+    val paparazzi = Paparazzi()
+
+    @TestParameter
+    val userDarkTheme: Boolean = false
+
+    @Test
+    fun renderTeamLetterFallback() {
+        val displayModel = TestDisplayModel.g2.copy(
+            imageUrl = ThemedImageURL(),
+        )
+
+        paparazzi.snapshotScreen(userDarkTheme) {
+            CircleTeamLogo(
+                displayModel = displayModel,
+                modifier = Modifier
+                    .size(96.dp),
+            )
+        }
+    }
+}

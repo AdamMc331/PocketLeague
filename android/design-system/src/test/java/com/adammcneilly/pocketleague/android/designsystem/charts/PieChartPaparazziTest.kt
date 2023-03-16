eab5eebbfa8866076ff734484d4c266fcedc1c08
Adam McNeilly <amcneilly331@gmail.com>
2023-03-15 20:24:36 -0400

    Adding paparazzi tests.

---
+++ android/design-system/src/test/java/com/adammcneilly/pocketleague/android/designsystem/charts/PieChartPaparazziTest.kt
@@ -0,0 +1,79 @@
+package com.adammcneilly.pocketleague.android.designsystem.charts
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import app.cash.paparazzi.Paparazzi
+import com.adammcneilly.pocketleague.android.designsystem.snapshotScreen
+import com.adammcneilly.pocketleague.android.designsystem.theme.rlcsBlue
+import com.adammcneilly.pocketleague.android.designsystem.theme.rlcsOrange
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class PieChartPaparazziTest {
+
+    @get:Rule
+    val paparazzi = Paparazzi()
+
+    @TestParameter
+    val useDarkTheme: Boolean = false
+
+    private val segments = listOf(
+        PieChartSegment(10, rlcsBlue),
+        PieChartSegment(5, rlcsOrange),
+    )
+
+    @Test
+    fun renderTwoSegmentsFilled() {
+        paparazzi.snapshotScreen(useDarkTheme) {
+            PieChart(
+                segments = segments,
+                segmentStyle = PieChartSegmentStyle.Fill,
+                modifier = Modifier
+                    .size(96.dp),
+            )
+        }
+    }
+
+    @Test
+    fun renderTwoSegmentsStroked() {
+        paparazzi.snapshotScreen(useDarkTheme) {
+            PieChart(
+                segments = segments,
+                segmentStyle = PieChartSegmentStyle.Stroke(8.dp),
+                modifier = Modifier
+                    .size(96.dp),
+            )
+        }
+    }
+
+    @Test
+    fun renderFilledAndAnimating() {
+        paparazzi.snapshotScreen(useDarkTheme) {
+            PieChart(
+                segments = segments,
+                segmentStyle = PieChartSegmentStyle.Fill,
+                percentageToRender = 0.5F,
+                modifier = Modifier
+                    .size(96.dp),
+            )
+        }
+    }
+
+    @Test
+    fun renderStrokedAndAnimating() {
+        paparazzi.snapshotScreen(useDarkTheme) {
+            PieChart(
+                segments = segments,
+                segmentStyle = PieChartSegmentStyle.Stroke(8.dp),
+                percentageToRender = 0.5F,
+                modifier = Modifier
+                    .size(96.dp),
+            )
+        }
+    }
+}

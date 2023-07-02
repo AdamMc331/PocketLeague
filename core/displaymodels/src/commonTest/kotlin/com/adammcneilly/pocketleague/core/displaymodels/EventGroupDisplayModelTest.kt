7338353abb6d2b28cd31a90625df1ee1ccbd2489
Adam McNeilly <amcneilly331@gmail.com>
2023-07-02 19:55:34 -0400

    Adding unit test for group mapping.

---
+++ core/displaymodels/src/commonTest/kotlin/com/adammcneilly/pocketleague/core/displaymodels/EventGroupDisplayModelTest.kt
@@ -0,0 +1,65 @@
+package com.adammcneilly.pocketleague.core.displaymodels
+
+import com.adammcneilly.pocketleague.core.displaymodels.test.springInvitationalForAllRegions
+import com.adammcneilly.pocketleague.core.displaymodels.test.worldChampionship
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class EventGroupDisplayModelTest {
+
+    @Test
+    fun mapFromListWithJustMajor() {
+        val worlds = EventSummaryDisplayModel.worldChampionship()
+
+        val expectedGroups = listOf(
+            EventGroupDisplayModel.Major(worlds),
+        )
+
+        val output = EventGroupDisplayModel.mapFromEventList(listOf(worlds))
+
+        assertEquals(expectedGroups, output)
+    }
+
+    @Test
+    fun mapFromListWithJustRegionals() {
+        val regionals = EventSummaryDisplayModel.springInvitationalForAllRegions()
+
+        val expectedGroups = listOf(
+            EventGroupDisplayModel.Regionals(regionals),
+        )
+
+        val output = EventGroupDisplayModel.mapFromEventList(regionals)
+
+        assertEquals(expectedGroups, output)
+    }
+
+    @Test
+    fun mapFromListEndingWithRegionals() {
+        val worlds = EventSummaryDisplayModel.worldChampionship()
+        val regionals = EventSummaryDisplayModel.springInvitationalForAllRegions()
+
+        val expectedGroups = listOf(
+            EventGroupDisplayModel.Major(worlds),
+            EventGroupDisplayModel.Regionals(regionals),
+        )
+
+        val output = EventGroupDisplayModel.mapFromEventList(listOf(worlds) + regionals)
+
+        assertEquals(expectedGroups, output)
+    }
+
+    @Test
+    fun mapFromListEndingWithMajor() {
+        val worlds = EventSummaryDisplayModel.worldChampionship()
+        val regionals = EventSummaryDisplayModel.springInvitationalForAllRegions()
+
+        val expectedGroups = listOf(
+            EventGroupDisplayModel.Regionals(regionals),
+            EventGroupDisplayModel.Major(worlds),
+        )
+
+        val output = EventGroupDisplayModel.mapFromEventList(regionals + worlds)
+
+        assertEquals(expectedGroups, output)
+    }
+}

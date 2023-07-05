5799dacf7a9b0ddd96fb115be1d9c2d7d3cabf17
Adam McNeilly <amcneilly331@gmail.com>
2023-07-05 12:38:39 -0400

    Adding docs.

--- shared/app/src/iosMain/kotlin/com/adammcneilly/pocketleague/shared/app/PocketLeagueAppViewController.ios.kt
+++ shared/app/src/iosMain/kotlin/com/adammcneilly/pocketleague/shared/app/PocketLeagueAppViewController.ios.kt
@@ -2,6 +2,11 @@ package com.adammcneilly.pocketleague.shared.app
 
 import androidx.compose.ui.window.ComposeUIViewController
 
+/**
+ * Creates a [ComposeUIViewController] which is used on iOS
+ * to render composable content. This is just an entry point
+ * into our [PocketLeagueApp].
+ */
 fun pocketLeagueAppViewController() = ComposeUIViewController {
     PocketLeagueApp()
 }

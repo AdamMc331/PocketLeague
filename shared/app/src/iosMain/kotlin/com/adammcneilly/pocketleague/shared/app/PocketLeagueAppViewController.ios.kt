8ca496000988c30cb8dc84913c4adf4f92365d1b
Adam McNeilly <amcneilly331@gmail.com>
2023-07-05 12:37:34 -0400

    Adding iOS files for UI.

---
+++ shared/app/src/iosMain/kotlin/com/adammcneilly/pocketleague/shared/app/PocketLeagueAppViewController.ios.kt
@@ -0,0 +1,7 @@
+package com.adammcneilly.pocketleague.shared.app
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun pocketLeagueAppViewController() = ComposeUIViewController {
+    PocketLeagueApp()
+}

642baa9a204bc00f8bcf0b30be57209a1bc953f9
Adam McNeilly <amcneilly331@gmail.com>
2023-07-06 19:03:23 -0400

    Implementing ios date formatting.

--- core/datetime/src/iosMain/kotlin/com/adammcneilly/pocketleague/core/datetime/IOSDateTimeFormatter.kt
+++ core/datetime/src/iosMain/kotlin/com/adammcneilly/pocketleague/core/datetime/IOSDateTimeFormatter.kt
@@ -1,17 +1,27 @@
 package com.adammcneilly.pocketleague.core.datetime
 
+import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toNSDate
+import platform.Foundation.NSDateFormatter
 
 /**
  * Coming soon!
  */
 class IOSDateTimeFormatter : DateTimeFormatter {
 
+    /**
+     * Inspiration: https://github.com/Kotlin/kotlinx-datetime/issues/211#issuecomment-1285745207
+     */
     override fun formatUTCString(
         utcString: String,
         formatPattern: String,
         timeZone: TimeZone,
-    ): String? {
-        return utcString
+    ): String {
+        val instant = Instant.parse(utcString)
+        val dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = formatPattern
+
+        return dateFormatter.stringFromDate(instant.toNSDate())
     }
 }

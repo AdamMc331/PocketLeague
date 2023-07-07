febb4bb048d51b76ed0da8eaa037c46d9d3c486a
Adam McNeilly <amcneilly331@gmail.com>
2023-07-07 18:39:41 -0400

    Adding paparazzi tests.

---
+++ shared/ui/src/test/kotlin/com/adammcneilly/pocketleague/shared/ui/match/MatchContentPaparazziTest.kt
@@ -0,0 +1,39 @@
+package com.adammcneilly.pocketleague.shared.ui.match
+
+import app.cash.paparazzi.Paparazzi
+import com.adammcneilly.pocketleague.core.displaymodels.MatchDetailDisplayModel
+import com.adammcneilly.pocketleague.core.displaymodels.test.TestDisplayModel
+import com.adammcneilly.pocketleague.shared.ui.snapshotScreen
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
+import org.junit.runner.RunWith
+import kotlin.test.Test
+
+@RunWith(TestParameterInjector::class)
+class MatchContentPaparazziTest {
+
+    @get:Rule
+    val paparazzi = Paparazzi()
+
+    @TestParameter
+    val useDarkTheme: Boolean = false
+
+    @Test
+    fun renderMatchDetailContent() {
+        paparazzi.snapshotScreen(useDarkTheme) {
+            MatchDetailContent(
+                match = TestDisplayModel.matchDetailBlueWinner,
+            )
+        }
+    }
+
+    @Test
+    fun renderPlaceholder() {
+        paparazzi.snapshotScreen(useDarkTheme) {
+            MatchDetailContent(
+                match = MatchDetailDisplayModel.placeholder,
+            )
+        }
+    }
+}

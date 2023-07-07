e476802f127fabf27172902565567137684c6477
Adam McNeilly <amcneilly331@gmail.com>
2023-07-07 17:10:53 -0400

    Removing per page req.

--- data/match/src/commonMain/kotlin/com/adammcneilly/pocketleague/data/match/OctaneGGMatchService.kt
+++ data/match/src/commonMain/kotlin/com/adammcneilly/pocketleague/data/match/OctaneGGMatchService.kt
@@ -26,8 +26,7 @@ class OctaneGGMatchService(
             octaneMatch.toMatch()
         }
     }
-
-    @Suppress("MagicNumber")
+movin
     override suspend fun getPastWeeksMatches(): Result<List<Match>> {
         return apiClient.getResponse<OctaneGGMatchListResponse>(
             endpoint = MATCHES_ENDPOINT,
@@ -35,7 +34,6 @@ class OctaneGGMatchService(
                 "before" to Clock.System.now(),
                 "after" to Clock.System.now().minus(NUM_DAYS_RECENT_MATCHES.days),
                 "group" to "rlcs",
-                "perPage" to 5, // Set to speed up API call because Amtrak wifi lmao
             ),
         ).map { octaneMatchListResponse ->
             val mappedMatches =

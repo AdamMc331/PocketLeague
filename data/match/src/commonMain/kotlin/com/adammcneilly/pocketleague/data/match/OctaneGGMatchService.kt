9874c8582561490f186ed28ed87cd068a0679cf3
Adam McNeilly <amcneilly331@gmail.com>
2023-07-07 17:11:04 -0400

    Removing per page req.

--- data/match/src/commonMain/kotlin/com/adammcneilly/pocketleague/data/match/OctaneGGMatchService.kt
+++ data/match/src/commonMain/kotlin/com/adammcneilly/pocketleague/data/match/OctaneGGMatchService.kt
@@ -26,7 +26,7 @@ class OctaneGGMatchService(
             octaneMatch.toMatch()
         }
     }
-movin
+
     override suspend fun getPastWeeksMatches(): Result<List<Match>> {
         return apiClient.getResponse<OctaneGGMatchListResponse>(
             endpoint = MATCHES_ENDPOINT,

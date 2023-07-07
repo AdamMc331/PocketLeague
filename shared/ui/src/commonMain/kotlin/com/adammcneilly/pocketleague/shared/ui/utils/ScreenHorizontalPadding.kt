60a8f57bbdd7d1cc58ef2e2df2d9f870690e5802
Adam McNeilly <amcneilly331@gmail.com>
2023-07-07 11:44:14 -0400

    Adding docs.

--- shared/ui/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/utils/ScreenHorizontalPadding.kt
+++ shared/ui/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/utils/ScreenHorizontalPadding.kt
@@ -5,6 +5,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import com.adammcneilly.pocketleague.shared.design.system.theme.PocketLeagueTheme
 
+/**
+ * A custom [Modifier] that applies the screen padding as the horizontal
+ * padding for some component.
+ *
+ * This is useful for situations like the feed screen, where the content is shown
+ * in a lazy list, and we don't want to apply horizontal padding to the list itself
+ * since that will cut off any carousels on the screen.
+ */
 fun Modifier.screenHorizontalPadding() = composed {
     this.padding(
         horizontal = PocketLeagueTheme.sizes.screenPadding,

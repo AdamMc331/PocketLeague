9c006491ca46eb066b8b182b136b707d7e244a1c
Adam McNeilly <amcneilly331@gmail.com>
2023-07-23 17:05:04 -0400

    Removing unused modules. (#325)

--- android/app/src/main/java/com/adammcneilly/pocketleague/ui/sizeconfigs/ContentType.kt
+++
@@ -1,9 +0,0 @@
-package com.adammcneilly.pocketleague.ui.sizeconfigs
-
-/**
- * Defines how content will be rendered within available space in the application.
- */
-enum class ContentType {
-    SINGLE_PANE,
-    DUAL_PANE,
-}
--- feature/event-detail/README.md
+++
@@ -1,5 +0,0 @@
-# Feature Event Detail
-
-## Overview
-
-The event detail feature shows information about a specific event inside the Pocket League app.

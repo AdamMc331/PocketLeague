54905c5ab545bfda47cdf34a51845d88f25f1b82
Adam McNeilly <amcneilly331@gmail.com>
2023-07-28 13:14:36 -0400

    Adding screenshots for placeholder changes.

--- pocketLeagueIos/pocketLeagueIos/pocketLeagueIosApp.swift
+++ pocketLeagueIos/pocketLeagueIos/pocketLeagueIosApp.swift
@@ -5,10 +5,16 @@
 //  Created by Adam McNeilly on 7/6/23.
 //
 
+import shared
 import SwiftUI
 
 @main
 struct pocketLeagueIosApp: App {
+    
+    init() {
+        // TODO: Initialize Koin
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()

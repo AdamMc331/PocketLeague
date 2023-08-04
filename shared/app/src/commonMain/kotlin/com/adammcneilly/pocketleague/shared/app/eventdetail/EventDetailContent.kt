58afba442478187013dc402fe660b5e7d656e4ab
Adam McNeilly <amcneilly331@gmail.com>
2023-08-04 13:37:43 -0400

    Working event stage pager.

--- shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/eventdetail/EventDetailContent.kt
+++ shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/eventdetail/EventDetailContent.kt
@@ -1,10 +1,13 @@
 package com.adammcneilly.pocketleague.shared.app.eventdetail
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +19,7 @@ import com.adammcneilly.pocketleague.shared.ui.components.ListItemDividerCard
 /**
  * UI representation of an [event].
  */
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun EventDetailContent(
     event: EventDetailDisplayModel,
@@ -24,17 +28,46 @@ fun EventDetailContent(
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(
-            PocketLeagueTheme.sizes.screenPadding,
+            vertical = PocketLeagueTheme.sizes.screenPadding,
         ),
         verticalArrangement = Arrangement.spacedBy(
             PocketLeagueTheme.sizes.listItemSpacing,
         ),
     ) {
-        stagesSection(event)
+        horizontalStageSection(event)
     }
 }
 
-private fun LazyListScope.stagesSection(event: EventDetailDisplayModel) {
+@OptIn(ExperimentalFoundationApi::class)
+private fun LazyListScope.horizontalStageSection(event: EventDetailDisplayModel) {
+    val stages = event.getStageSummaries()
+
+    item {
+        val pagerState = rememberPagerState()
+        val pageSpacingDp = PocketLeagueTheme.sizes.listItemSpacing
+
+        HorizontalPager(
+            pageCount = stages.size,
+            pageSpacing = PocketLeagueTheme.sizes.listItemSpacing,
+            contentPadding = PaddingValues(
+                horizontal = pageSpacingDp.times(2),
+            ),
+            state = pagerState,
+        ) { pageIndex ->
+            val stage = stages[pageIndex]
+
+            // Based on page index, could we modify the card's full width?
+
+            EventStageCard(
+                eventStage = stage,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            )
+        }
+    }
+}
+
+private fun LazyListScope.verticalStagesSection(event: EventDetailDisplayModel) {
     val stages = event.getStageSummaries()
 
     if (stages.isEmpty()) {
---
+++ shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/eventdetail/EventStageCard.kt
@@ -0,0 +1,46 @@
+package com.adammcneilly.pocketleague.shared.app.eventdetail
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.adammcneilly.pocketleague.core.displaymodels.EventStageSummaryDisplayModel
+import com.adammcneilly.pocketleague.shared.design.system.theme.PocketLeagueTheme
+
+/**
+ * A minimal [Card] component to highlight information about
+ * an [eventStage].
+ */
+@Composable
+fun EventStageCard(
+    eventStage: EventStageSummaryDisplayModel,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(PocketLeagueTheme.sizes.textSpacing),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(PocketLeagueTheme.sizes.cardPadding),
+        ) {
+            Text(
+                text = eventStage.name,
+                style = MaterialTheme.typography.headlineLarge,
+            )
+
+            Text(
+                text = eventStage.dateString,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+        }
+    }
+}

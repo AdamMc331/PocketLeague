ddf66ad203f3b18f6441636cb1b8e4a631a54831
Adam McNeilly <amcneilly331@gmail.com>
2023-08-04 12:21:55 -0400

    Fetching event detail when presenter is created.

--- shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/eventdetail/EventDetailContent.kt
+++ shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/eventdetail/EventDetailContent.kt
@@ -14,6 +14,6 @@ fun EventDetailContent(
     modifier: Modifier = Modifier,
 ) {
     Text(
-        text = "Event Detail Screen",
+        text = "Event Detail Screen; Event name: ${event.name}",
     )
 }
--- shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/eventdetail/EventDetailPresenter.kt
+++ shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/eventdetail/EventDetailPresenter.kt
@@ -1,14 +1,19 @@
 package com.adammcneilly.pocketleague.shared.app.eventdetail
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.adammcneilly.pocketleague.core.displaymodels.EventDetailDisplayModel
+import com.adammcneilly.pocketleague.core.displaymodels.toDetailDisplayModel
 import com.adammcneilly.pocketleague.core.models.Event
 import com.adammcneilly.pocketleague.data.event.EventRepository
 import com.slack.circuit.runtime.presenter.Presenter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 
 /**
  * State management container for the event detail screen.
@@ -22,6 +27,16 @@ class EventDetailPresenter(
     override fun present(): EventDetailScreen.State {
         var displayModel by remember {
             mutableStateOf(EventDetailDisplayModel.placeholder)
+        }
+
+        LaunchedEffect(Unit) {
+            eventRepository
+                .getEvent(eventId)
+                .map(Event::toDetailDisplayModel)
+                .onEach { eventDetail ->
+                    displayModel = eventDetail
+                }
+                .launchIn(this)
         }
 
         return EventDetailScreen.State(

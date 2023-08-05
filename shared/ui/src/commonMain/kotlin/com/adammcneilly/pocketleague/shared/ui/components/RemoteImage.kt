91d55c03a32e4ed1f11fdc3858403d01a203032c
Adam McNeilly <amcneilly331@gmail.com>
2023-08-05 17:22:25 -0400

    Docs.

--- shared/ui/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/components/RemoteImage.kt
+++ shared/ui/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/components/RemoteImage.kt
@@ -3,6 +3,9 @@ package com.adammcneilly.pocketleague.shared.ui.components
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+/**
+ * Displays an image from a remote [imageUrl].
+ */
 @Composable
 expect fun RemoteImage(
     imageUrl: String,

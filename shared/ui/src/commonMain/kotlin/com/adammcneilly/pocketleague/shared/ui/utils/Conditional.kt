ceeb984814a32acf801a9cdea78e08d190935a62
Adam McNeilly <amcneilly331@gmail.com>
2023-08-05 20:56:59 -0400

    Docs.

--- shared/ui/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/utils/Conditional.kt
+++ shared/ui/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/utils/Conditional.kt
@@ -2,6 +2,9 @@ package com.adammcneilly.pocketleague.shared.ui.utils
 
 import androidx.compose.ui.Modifier
 
+/**
+ * Conditionally applies the supplied [modifier] only if the [condition] is true.
+ */
 fun Modifier.conditional(
     condition: Boolean,
     modifier: Modifier.() -> Modifier,

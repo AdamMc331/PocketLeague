7096c692155380053158d80975ce0e322a746110
Adam McNeilly <amcneilly331@gmail.com>
2023-08-09 19:54:04 -0400

    Changing header component to list item.

---
+++ feature/teamdetail/src/commonMain/kotlin/com/adammcneilly/pocketleague/feature/teamdetail/TeamHeaderListItem.kt
@@ -0,0 +1,42 @@
+package com.adammcneilly.pocketleague.feature.teamdetail
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.adammcneilly.pocketleague.core.displaymodels.TeamOverviewDisplayModel
+import com.adammcneilly.pocketleague.shared.ui.components.CircleTeamLogo
+
+/**
+ * Component that renders overview information about a team (logo, name, region)
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TeamHeaderListItem(
+    team: TeamOverviewDisplayModel,
+    modifier: Modifier = Modifier,
+) {
+    ListItem(
+        modifier = modifier,
+        leadingContent = {
+            CircleTeamLogo(
+                displayModel = team,
+                modifier = Modifier
+                    .size(48.dp),
+            )
+        },
+        headlineText = {
+            Text(
+                text = team.name,
+            )
+        },
+        supportingText = {
+            Text(
+                text = team.region.name,
+            )
+        },
+    )
+}

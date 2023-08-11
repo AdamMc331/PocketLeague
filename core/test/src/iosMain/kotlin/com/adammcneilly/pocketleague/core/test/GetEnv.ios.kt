d66c51fa386afa74684923fb298d3ce84c7e936a
Adam McNeilly <amcneilly331@gmail.com>
2023-08-11 12:19:42 -0400

    Fixing test compilation.

--- core/test/src/iosMain/kotlin/com/adammcneilly/pocketleague/core/test/GetEnv.ios.kt
+++ core/test/src/iosMain/kotlin/com/adammcneilly/pocketleague/core/test/GetEnv.ios.kt
@@ -1,7 +1,9 @@
 package com.adammcneilly.pocketleague.core.test
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.toKString
 import platform.posix.getenv
 
+@OptIn(ExperimentalForeignApi::class)
 internal actual fun getEnv(name: String): String? =
     getenv(name)?.toKString()

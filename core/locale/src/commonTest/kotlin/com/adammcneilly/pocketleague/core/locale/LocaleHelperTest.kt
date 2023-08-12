6c512028eb77a86655a8b73275fcf2216d3847e7
Adam McNeilly <amcneilly331@gmail.com>
2023-08-12 12:02:04 -0400

    Moving LocaleHelperTest to commonTest.

--- core/locale/src/commonTest/kotlin/com/adammcneilly/pocketleague/core/locale/LocaleHelperTest.kt
+++ core/locale/src/commonTest/kotlin/com/adammcneilly/pocketleague/core/locale/LocaleHelperTest.kt
@@ -4,7 +4,7 @@ import com.varabyte.truthish.assertThat
 import kotlin.test.Test
 
 class LocaleHelperTest {
-    private val localeHelper = JVMLocaleHelper()
+    private val localeHelper = provideLocaleHelper()
 
     @Test
     fun getCountryDisplayName() {
@@ -17,5 +17,14 @@ class LocaleHelperTest {
     fun getCountryFlag() {
         assertThat(localeHelper.getFlagEmoji("US")).isEqualTo("\uD83C\uDDFA\uD83C\uDDF8")
         assertThat(localeHelper.getFlagEmoji("FR")).isEqualTo("\uD83C\uDDEB\uD83C\uDDF7")
+    }
+
+    @Test
+    fun overrideEnglandFlag() {
+        assertThat(
+            actual = localeHelper.getFlagEmoji("EN")
+        ).isEqualTo(
+            expected = "\uD83C\uDFF4\uDB40\uDC67\uDB40\uDC62\uDB40\uDC65\uDB40\uDC6E\uDB40\uDC67\uDB40\uDC7F"
+        )
     }
 }

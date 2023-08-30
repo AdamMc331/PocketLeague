a1d6a16118337b4d3942414a496a4068d418189b
Adam McNeilly <amcneilly331@gmail.com>
2023-08-30 19:31:40 -0400

    Removing Compose BOM. (#378)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -29,20 +30,14 @@ android {
             )
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
 
     packaging {
@@ -53,17 +48,14 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.compose.bom))
-
     implementation(projects.shared.app)
     implementation(projects.shared.designSystem)
+    implementation(compose.foundation)
+    implementation(compose.material3)
     implementation(libs.accompanist.systemuicontroller)
     implementation(libs.activity.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.compose.material3)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.ui.tooling.preview)
     implementation(libs.koin.android)
     implementation(libs.ui.graphics)
 
@@ -71,9 +63,4 @@ dependencies {
 
     androidTestImplementation(libs.androidx.test.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.compose.ui.test.junit)
-
-    debugImplementation(libs.compose.ui.test.manifest)
-    debugImplementation(libs.compose.ui.tooling)
 }

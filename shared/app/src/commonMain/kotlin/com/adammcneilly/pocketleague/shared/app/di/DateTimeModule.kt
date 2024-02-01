904f5ab040f8f45a5725382986dbd9e7cb5a83df
Adam McNeilly <amcneilly331@gmail.com>
2024-01-31 21:01:00 -0500

    Conditionally returning time provider.

--- shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/di/DateTimeModule.kt
+++ shared/app/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/app/di/DateTimeModule.kt
@@ -1,18 +1,27 @@
 package com.adammcneilly.pocketleague.shared.app.di
 
 import com.adammcneilly.pocketleague.core.datetime.DateTimeFormatter
+import com.adammcneilly.pocketleague.core.datetime.DebugTimeProvider
 import com.adammcneilly.pocketleague.core.datetime.SystemTimeProvider
 import com.adammcneilly.pocketleague.core.datetime.TimeProvider
 import com.adammcneilly.pocketleague.core.datetime.dateTimeFormatter
+import com.adammcneilly.pocketleague.feature.debugmenu.DebugPreferences
 import org.koin.dsl.module
 
 /**
  * Defines any dependencies related to date and time management.
  */
 val dateTimeModule = module {
-    single<TimeProvider> {
-        SystemTimeProvider
-//        DebugTimeProvider()
+    factory<TimeProvider> {
+        val preferences: DebugPreferences = get()
+
+        if (preferences.useSystemTimeProvider) {
+            SystemTimeProvider
+        } else {
+            DebugTimeProvider(
+                dateString = preferences.debugTimeProviderDate,
+            )
+        }
     }
 
     single<DateTimeFormatter> {

0e41602d9d536c519fe5eb0b4b48d18d4ae2a895
Adam McNeilly <amcneilly331@gmail.com>
2024-11-26 15:27:30 -0500

    I might be making things worse.

--- data/event/build.gradle.kts
+++ data/event/build.gradle.kts
@@ -33,24 +33,7 @@ kotlin {
                 implementation(libs.kotlinx.coroutines.test)
             }
         }
-        maybeCreate("iosX64Main")
-        maybeCreate("iosArm64Main")
-        maybeCreate("iosSimulatorArm64Main")
-        maybeCreate("iosMain").apply {
-            dependsOn(commonMain)
-            getAt("iosX64Main").dependsOn(this)
-            getAt("iosArm64Main").dependsOn(this)
-            getAt("iosSimulatorArm64Main").dependsOn(this)
-        }
-        maybeCreate("iosX64Test")
-        maybeCreate("iosArm64Test")
-        maybeCreate("iosSimulatorArm64Test")
-        maybeCreate("iosTest").apply {
-            dependsOn(commonTest)
-            getAt("iosX64Test").dependsOn(this)
-            getAt("iosArm64Test").dependsOn(this)
-            getAt("iosSimulatorArm64Test").dependsOn(this)
-        }
+        
     }
 }
 
--- feature/teamdetail/build.gradle.kts
+++ feature/teamdetail/build.gradle.kts
@@ -38,24 +38,6 @@ kotlin {
                 implementation(libs.google.testparameterinjector)
             }
         }
-        maybeCreate("iosX64Main")
-        maybeCreate("iosArm64Main")
-        maybeCreate("iosSimulatorArm64Main")
-        maybeCreate("iosMain").apply {
-            dependsOn(commonMain)
-            getAt("iosX64Main").dependsOn(this)
-            getAt("iosArm64Main").dependsOn(this)
-            getAt("iosSimulatorArm64Main").dependsOn(this)
-        }
-        maybeCreate("iosX64Test")
-        maybeCreate("iosArm64Test")
-        maybeCreate("iosSimulatorArm64Test")
-        maybeCreate("iosTest").apply {
-            dependsOn(commonTest)
-            getAt("iosX64Test").dependsOn(this)
-            getAt("iosArm64Test").dependsOn(this)
-            getAt("iosSimulatorArm64Test").dependsOn(this)
-        }
     }
 
     targets.configureEach {
@@ -85,23 +67,3 @@ android {
 
     namespace = "com.adammcneilly.pocketleague.feature.teamdetail"
 }
-
-project.extensions.findByType(org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension::class.java)
-    ?.apply {
-        if (project.findProperty("ios") == "true") {
-            listOf(
-                iosX64(),
-                iosArm64(),
-                iosSimulatorArm64()
-            ).forEach {
-                it.binaries.framework {
-                    baseName = project.name
-                }
-            }
-        }
-        if (project.findProperty("js") == "true") {
-            js(IR) {
-                browser()
-            }
-        }
-    }

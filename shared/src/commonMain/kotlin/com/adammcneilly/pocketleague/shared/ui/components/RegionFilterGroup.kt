36855ae8e50820a33347bfbe1d270e35599c8c90
Adam McNeilly <amcneilly331@gmail.com>
2025-09-22 23:30:20 -0400

    Updating snapshots.

--- shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/components/RegionFilterGroup.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/components/RegionFilterGroup.kt
@@ -1,7 +1,10 @@
 package com.adammcneilly.pocketleague.shared.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,25 +16,32 @@ import com.adammcneilly.pocketleague.shared.models.Region
 fun RegionFilterGroup(
     selectedRegions: List<Region>,
     modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(
+        start = 16.dp,
+        end = 16.dp,
+    ),
 ) {
     val allSelected = (selectedRegions == Region.entries)
 
-    Row(
+    LazyRow(
         modifier = modifier,
+        contentPadding = contentPadding,
         horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        FilterChip(
-            selected = allSelected,
-            onClick = {
-            },
-            label = {
-                Text(
-                    text = "All",
-                )
-            },
-        )
+        item {
+            FilterChip(
+                selected = allSelected,
+                onClick = {
+                },
+                label = {
+                    Text(
+                        text = "All",
+                    )
+                },
+            )
+        }
 
-        Region.entries.forEach { region ->
+        items(Region.entries) { region ->
             FilterChip(
                 selected = !allSelected && selectedRegions.contains(region),
                 onClick = {

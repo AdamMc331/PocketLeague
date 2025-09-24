c644a1c8191bec0bd3009bac8e1586974c7feb3e
Adam McNeilly <amcneilly331@gmail.com>
2025-09-23 22:41:23 -0400

    Parcelable support.

--- shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/scaffold/navigation/NavItem.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/pocketleague/shared/ui/scaffold/navigation/NavItem.kt
@@ -1,12 +1,16 @@
 package com.adammcneilly.pocketleague.shared.ui.scaffold.navigation
 
+import com.adammcneilly.pocketleague.shared.Parcelable
+import com.adammcneilly.pocketleague.shared.Parcelize
+
 /**
  * Represents an item in a navigation component, such as a tab bar.
  *
  * @property[tab] The specific tab that this item represents.
  * @property[selected] If true, this navigation item is currently selected.
  */
+@Parcelize
 data class NavItem(
     val tab: HomeTab,
     val selected: Boolean,
-)
+) : Parcelable

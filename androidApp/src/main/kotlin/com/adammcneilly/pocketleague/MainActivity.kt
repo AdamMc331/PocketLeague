aefa8bb7033da21dc24fd285db0b71f831d81d64
Adam McNeilly <amcneilly331@gmail.com>
2025-09-23 21:45:09 -0400

    Removing saveable for now.

--- androidApp/src/main/kotlin/com/adammcneilly/pocketleague/MainActivity.kt
+++ androidApp/src/main/kotlin/com/adammcneilly/pocketleague/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import com.adammcneilly.pocketleague.navigation.AppNavHost
 import com.adammcneilly.pocketleague.shared.ui.scaffold.app.App
@@ -18,7 +19,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             enableEdgeToEdge()
 
-            val appState = rememberSaveable(saver = AppState.saver) {
+            // Commented out because I can't figure out the parcelable KMP stuff
+            // yet.
+//            val appState = rememberSaveable(saver = AppState.saver) {
+            val appState = remember {
                 AppState()
             }
 

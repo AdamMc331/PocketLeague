fba31f520562de4d18bb714b04ea2f150fa6e2bc
Adam McNeilly <amcneilly331@gmail.com>
2025-09-23 22:06:02 -0400

    Fixing iOS Build.

--- shared/src/iosMain/kotlin/com/adammcneilly/pocketleague/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/adammcneilly/pocketleague/shared/MainViewController.kt
@@ -1,6 +1,12 @@
 package com.adammcneilly.pocketleague.shared
 
+import androidx.compose.material3.Text
 import androidx.compose.ui.window.ComposeUIViewController
 
 @Suppress("ktlint:standard:function-naming", "FunctionNaming")
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() =
+    ComposeUIViewController {
+        Text(
+            text = "iOS Support Coming Soon",
+        )
+    }

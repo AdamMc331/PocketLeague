84cc0e101ed4ef4e717561c2f2282287a361e0d2
Adam McNeilly <amcneilly331@gmail.com>
2025-12-28 21:01:20 -0500

    Test screen implemented.

--- androidApp/src/main/kotlin/com/adammcneilly/pocketleague/navigation/TwoPaneSceneStrategy.kt
+++ androidApp/src/main/kotlin/com/adammcneilly/pocketleague/navigation/TwoPaneSceneStrategy.kt
@@ -1,23 +1,22 @@
 package com.adammcneilly.pocketleague.navigation
 
-import androidx.compose.runtime.Composable
 import androidx.navigation3.runtime.NavEntry
-import androidx.navigation3.ui.Scene
-import androidx.navigation3.ui.SceneStrategy
+import androidx.navigation3.scene.Scene
+import androidx.navigation3.scene.SceneStrategy
+import androidx.navigation3.scene.SceneStrategyScope
 import com.adammcneilly.pocketleague.shared.ui.utils.isMediumScreenWidthOrWider
 
 /**
  * An implementation of [SceneStrategy] that determines if two [AppScreen] entries can appear in a
  * two pane scene together.
  */
-class TwoPaneSceneStrategy : SceneStrategy<AppScreen> {
-    @Composable
-    @Suppress("ReturnCount")
-    override fun calculateScene(
+class TwoPaneSceneStrategy(
+    private val isMediumOrLargerWidth: Boolean,
+) : SceneStrategy<AppScreen> {
+    override fun SceneStrategyScope<AppScreen>.calculateScene(
         entries: List<NavEntry<AppScreen>>,
-        onBack: (Int) -> Unit,
     ): Scene<AppScreen>? {
-        if (isMediumScreenWidthOrWider().value) {
+        if (isMediumOrLargerWidth) {
             return null
         }
 

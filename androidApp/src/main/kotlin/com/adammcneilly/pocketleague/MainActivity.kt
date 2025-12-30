add14a292a03521055d537a69a751459b3bafdc2
Adam McNeilly <amcneilly331@gmail.com>
2025-12-29 22:27:37 -0500

    Removing old nav3 code.

--- androidApp/src/main/kotlin/com/adammcneilly/pocketleague/MainActivity.kt
+++ androidApp/src/main/kotlin/com/adammcneilly/pocketleague/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.saveable.rememberSaveable
-import com.adammcneilly.pocketleague.navigation.AppNavHost
 import com.adammcneilly.pocketleague.shared.ui.scaffold.app.App
-import com.adammcneilly.pocketleague.shared.ui.scaffold.app.AppState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(
